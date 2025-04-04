0b9d5aeed9d76268c55211ff604041427c239005
hildanku <hildankutomo@gmail.com>
2025-04-05 01:17:26 +0700

    fix: wrong call function when want to call mergeSort - fix while block scope in mergeSort function - WIP: fix bug left and right number is same, expected when right and   left numer is same, it will return empty array (not pushed to result   array)

--- src/algorithms/sorts/2-merge-sort.ts
+++ src/algorithms/sorts/2-merge-sort.ts
@@ -2,7 +2,7 @@ function mergeSort(a: number[], b: number[]): number[] {
 
     let result = []
 
-    while (a && b != null) {
+    while (a.length > 0 && b.length > 0) {
         if (a[0] <= b[0]) {
             result.push(a.shift()!)
         } else {
@@ -17,20 +17,23 @@ function mergeSort(a: number[], b: number[]): number[] {
     while (b.length < 0) {
         result.push(b.shift()!)
     }
+
+    console.log("splitting", result)
     return result
 }
 
 function merge(arr: number[]) {
 
     if (arr.length <= 1) return arr
+
     const middle = Math.floor(arr.length / 2)
     const left = arr.slice(0, middle)
     const right = arr.slice(middle)
+    console.log('merge', left, right)
+    //const sortedLeft = merge(left)
+    //const sortedRight = merge(right)
 
-    const sortedLeft = merge(left)
-    const sortedRight = merge(right)
-
-    return mergeSort(sortedLeft, sortedRight)
+    return mergeSort(left, right)
 }
 
 console.log(merge([5, 6, 2, 6, 7, 9, 1, 2, 6]))
